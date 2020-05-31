35d4a8f523b4e08fe451effeafa4ff03935b4ec8
javaComSci <iramanat@purdue.edu>
2020-05-31 16:31:05 -0400

    converstions to text

--- Backend/Controllers/DefinitionController.cs
+++ Backend/Controllers/DefinitionController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Backend.Models;
+using System.Diagnostics;
 
 
 namespace Backend.Controllers
@@ -20,9 +21,27 @@ namespace Backend.Controllers
             string audioFile = definitioninfo.definitionaudio;
             string trimmedAudio = audioFile.Substring(audioFile.LastIndexOf(',') + 1);
 
-            // get information from speech recognization engine
-            
+            string convertedText = "";
 
+            // get information from speech recognization engine
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = "/Users/indhu/anaconda3/bin/python";
+            start.Arguments = "Scripts/SpeechRecognition.py " + trimmedAudio;
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            using(Process process = Process.Start(start)) {
+                using(StreamReader reader = process.StandardOutput) {
+                    string result = reader.ReadToEnd();
+                    if(result.Equals("Cannot understand audio")){
+                        convertedText = "";
+                    } else {
+                        convertedText = result.Substring(result.LastIndexOf(":") + 1);
+                        Console.WriteLine("CONVERTED TEXT " + result);
+                    }
+                }
+            }
         }
+
+
     }
 }
--- Backend/Scripts/SpeechRecognition.py
+++ Backend/Scripts/SpeechRecognition.py
@@ -1,15 +1,36 @@
 import speech_recognition as sr
+import os
+import sys
+import base64
 from pydub import AudioSegment
 
 
-sound = AudioSegment.from_mp3("/path/to/file.mp3")
-sound.export("/output/path/file.wav", format="wav")
+if __name__ == "__main__":
+    # get encoded string
+    encodedString = sys.argv[1]
 
-r = sr.Recognizer()
-with sr.WavFile("speech.wav") as source: 
-    audio = r.record(source)
+    # get the location of where the files are
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    mp3_file_location = os.path.join(__location__, 'speech.mp3')
+    wav_file_location = os.path.join(__location__, 'speech.wav')
 
-try:
-    print("Transcription: " + r.recognize_google(audio))
-except sr.UnknownValueError: 
-    print("Cannot understand audio") 
+    # convert the encoded audio into mp3 file
+    mp3_file = open(mp3_file_location, "wb")
+    decodedString = base64.b64decode(encodedString)
+    mp3_file.write(decodedString)
+    mp3_file.close()
+
+    # convert mp3 file to wav file
+    mp3_segment = AudioSegment.from_mp3(mp3_file_location)
+    mp3_segment.export(wav_file_location, format="wav")
+
+    # get the audio from source
+    r = sr.Recognizer()
+    with sr.WavFile(wav_file_location) as source: 
+        audio = r.record(source)
+
+    # convert the audio
+    try:
+        print("Transcription: " + r.recognize_google(audio))
+    except sr.UnknownValueError: 
+        print("Cannot understand audio") 
